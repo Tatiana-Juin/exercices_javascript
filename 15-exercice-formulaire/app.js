92a88d6957a74dcce4326b6e850e2df772b12132
Tatiana-Juin <tatiana.juinp15@gmail.com>
2023-07-10 15:00:01 +0200

    tous fonctionne en js . la seule chose que je n'ai pas faite est le slider

--- 15-exercice-formulaire/app.js
+++ 15-exercice-formulaire/app.js
@@ -1,9 +1,51 @@
 // Recuperer les elements 
-let h1 =  document.querySelector("h1");
+let titre =  document.querySelector("h1");
 let connexion = document.querySelector("#connexion");
 let inscription = document.querySelector("#inscription");
-let btnConnexion = document.querySelector("#btnConnexion");
-let btnInscription = document.querySelector("#btnInscription");
+let nonMembre = document.querySelector("#nonMembre");
+let creerCompte = document.querySelector("#nonMembre a");
 let formConnexion = document.querySelector("#formConnexion");
 let formInscription = document.querySelector("#formInscription");
+
+
+//function pour afficher le formulaire d'inscription ainsi que le bon titre
+function funcInscription(){
+     //changement du titre
+   // titre.innerText="";
+   titre.innerText="Inscription";
+   //supprimer un attribut de connexion
+   connexion.removeAttribute("class");
+   //ajouter un attribut a inscription
+   inscription.setAttribute("class","active");
+
+   //changer la valeur d'un attribut
+   formConnexion.style.display = "none";
+   nonMembre.style.display = "none";
+   formInscription.style.display = "flex";
+}
+
+//function pour afficher le formulaire de connexion ainsi que le bon titre
+function funcConnexion(){
+    //on change le titre 
+    titre.innerText="Connexion";
+    //on supprime la class de inscription afin d'enlever le background-color sur le bouton inscription
+    inscription.removeAttribute("class");
+    //on ajoute la class active
+    connexion.setAttribute("class","active");
+
+    //on cache le formulaire inscription
+    formInscription.style.display = "none";
+    //on rend visible le formulaire et le p de inscription
+    formConnexion.style.display = "flex";
+    nonMembre.style.display = "block";
+
+}
+
+//evenement sur inscription - appelle et execution de la fonction funcInscription()
+inscription.addEventListener("click",funcInscription);
+
+//evenement sur connexion - appelle et execution de la fonction funcConnexion()
+connexion.addEventListener("click", funcConnexion);
+//evenment sur creerCompte - quand une personne va cliquer sur le lien creer un compte déclenche la function funcInscription 
+creerCompte.addEventListener("click", funcInscription);
 
