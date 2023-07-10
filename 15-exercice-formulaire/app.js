00e4ea71c7d3882c0ee00d37a17f8ae6e34cbd78
Tatiana-Juin <tatiana.juinp15@gmail.com>
2023-07-10 14:12:26 +0200

    les boutons connexion et inscription ont le bon style

---
+++ 15-exercice-formulaire/app.js
@@ -0,0 +1,9 @@
+// Recuperer les elements 
+let h1 =  document.querySelector("h1");
+let connexion = document.querySelector("#connexion");
+let inscription = document.querySelector("#inscription");
+let btnConnexion = document.querySelector("#btnConnexion");
+let btnInscription = document.querySelector("#btnInscription");
+let formConnexion = document.querySelector("#formConnexion");
+let formInscription = document.querySelector("#formInscription");
+
