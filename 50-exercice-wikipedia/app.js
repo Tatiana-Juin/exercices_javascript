8e0706bd6f7acd955f84db156d8244277dc9c81d
Tatiana-Juin <tatiana.juinp15@gmail.com>
2023-11-08 17:40:01 +0100

    appelle pour récupérer les éléments ecris fonctionne

--- 50-exercice-wikipedia/app.js
+++ 50-exercice-wikipedia/app.js
@@ -1,1 +1,18 @@
 //`https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=
+
+let erreur = document.querySelector(".erreur");
+let champRecherche = document.querySelector(".champ-recherche");
+let resultat = document.querySelector(".resultat");
+let form = document.querySelector(".recherche");
+
+//appelle de API 
+const API = "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=";
+
+//fonction asynchrone pour récuperer plusieurs donnée 
+async function dataWikipedia(recherche){
+    const reponse = await fetch(`${API}${recherche}`);
+    const data = await reponse.json();
+    console.log(data);
+}
+//appelle de la fonction asynchrone fonctionne
+dataWikipedia("github");
