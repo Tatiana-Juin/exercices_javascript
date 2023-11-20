916352ab52265a5e10acc8a3c572099a642c7a20
Tatiana-Juin <tatiana.juinp15@gmail.com>
2023-11-20 19:24:00 +0100

    debut de la barre de recherche et un peu de code js juste le minimum . Il reste à faire la card pokemon

---
+++ 51-exercice-pokeapi/app.js
@@ -0,0 +1,8 @@
+// RECUPERATION THE DOM ELEMENTS
+const search = document.querySelector("#search");
+const form = document.querySelector("form");
+
+form.addEventListener("submit",(e) =>{
+    e.preventDefault();
+
+})
