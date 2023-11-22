1cb710f223a696f8eb84b93b6a16bf62d40783ff
Tatiana-Juin <tatiana.juinp15@gmail.com>
2023-11-22 12:59:05 +0100

    debut de la mise en page des résultat pour la recherche du pokemon

--- 51-exercice-pokeapi/app.js
+++ 51-exercice-pokeapi/app.js
@@ -1,7 +1,7 @@
 // RECUPERATION THE DOM ELEMENTS
-const search = document.querySelector("#search");
-const form = document.querySelector("form");
-
+let search = document.querySelector("#search");
+let form = document.querySelector("form");
+let card = document.querySelector(".card-pokemon");
 //appelle de l'API
 let API = "https://pokeapi.co/api/v2/pokemon/";
 
@@ -9,22 +9,41 @@ let API = "https://pokeapi.co/api/v2/pokemon/";
 async function dataPokemon(name) {
     const reponse = await fetch(`${API}${name}`);
     const data = await reponse.json()
-    console.log(data);
+    // console.log(data);
     affichage(data);
 }
 
 
-
+// fonction pour afficher un pokemon 
 function affichage(name) {
-    // console.log(name);
-    let nameSearch = name["name"];
-    let id = name["id"];
-    let picture = name['sprites']['back_default']
-    
+
+    // image 
+    // let picture = name['sprites']['front_default']
+    let img = document.createElement("img");
+    img.setAttribute("src",name['sprites']['front_default']);
+    img.setAttribute("class","picture");
+    card.appendChild(img);
+    // nom 
+    // let nameSearch = name["name"];
+    let h2 = document.createElement("h2");
+    h2.innerText = name["name"];
+    h2.setAttribute("class","name-pokemon");
+    card.appendChild(h2);
+    // id
+    // let id = name["id"];
+    let paraId = document.createElement("p");
+    paraId.innerText = name["id"];
+    paraId.setAttribute("class","id-pokemon")
+    card.appendChild(paraId);
+
     // boucle pour récupérer le type 
     for (let i = 0; i< name["types"].length;i++){
-        let type = name["types"][i].type.name;
-        // console.log(type);
+        // let type = name["types"][i].type.name;
+        let paraType = document.createElement("p");
+        paraType.innerHTML= ` Type : ${name["types"][i].type.name}`
+        paraType.setAttribute("class","type-pokemon")
+        card.appendChild(paraType);
+        
     }
 
     //   console.log(type);
@@ -43,8 +62,11 @@ form.addEventListener("submit",(e) =>{
     e.preventDefault();
 
     if(search.value.length > 0){
+        card.textContent="";
+        // search.innerHTML=""
         dataPokemon(search.value);
         // console.log(dataPokemon);
+        
       
     }
 
