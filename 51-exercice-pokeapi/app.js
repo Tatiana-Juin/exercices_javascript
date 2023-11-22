0a4a7242805d06ed19f4b33981ff05e02cb8b582
Tatiana-Juin <tatiana.juinp15@gmail.com>
2023-11-22 12:10:49 +0100

    Pour chaque pokemon j'ai récupérer les info dans les console.log

--- 51-exercice-pokeapi/app.js
+++ 51-exercice-pokeapi/app.js
@@ -2,7 +2,50 @@
 const search = document.querySelector("#search");
 const form = document.querySelector("form");
 
+//appelle de l'API
+let API = "https://pokeapi.co/api/v2/pokemon/";
+
+// function asynchrone pour appeller API et récupérer des donnée
+async function dataPokemon(name) {
+    const reponse = await fetch(`${API}${name}`);
+    const data = await reponse.json()
+    console.log(data);
+    affichage(data);
+}
+
+
+
+function affichage(name) {
+    // console.log(name);
+    let nameSearch = name["name"];
+    let id = name["id"];
+    let picture = name['sprites']['back_default']
+    
+    // boucle pour récupérer le type 
+    for (let i = 0; i< name["types"].length;i++){
+        let type = name["types"][i].type.name;
+        // console.log(type);
+    }
+
+    //   console.log(type);
+
+    //pour pour récupérer toutes les statistique
+    for (let j = 0; j<name["stats"].length;j++){
+        let nameStat = name["stats"][j]["stat"].name
+        // console.log(nameStat);
+        let statistic = name["stats"][j].base_stat;
+        // console.log(statistic);
+    }
+ }
+
+// fonction de recherche 
 form.addEventListener("submit",(e) =>{
     e.preventDefault();
+
+    if(search.value.length > 0){
+        dataPokemon(search.value);
+        // console.log(dataPokemon);
+      
+    }
 
 })
