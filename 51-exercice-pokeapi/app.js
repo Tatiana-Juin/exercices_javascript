57a06f8602dfc254081c91e12084b664125a2c14
Tatiana-Juin <tatiana.juinp15@gmail.com>
2023-11-23 11:22:19 +0100

    creation d'un div pour l'image et j'ai fais en sorte que le texte ce supprime dee la barre de recherche quand la recherche à était faites

--- 51-exercice-pokeapi/app.js
+++ 51-exercice-pokeapi/app.js
@@ -17,20 +17,24 @@ async function dataPokemon(name) {
 // fonction pour afficher un pokemon 
 function affichage(name) {
 
+    // creation d'une div qui va contenir l'image 
+    let divImg = document.createElement("div");
+    divImg.setAttribute("class","card-img-pokemon");
+    card.appendChild(divImg)
+
     // image 
-    // let picture = name['sprites']['front_default']
     let img = document.createElement("img");
     img.setAttribute("src",name['sprites']['front_default']);
     img.setAttribute("class","picture");
-    card.appendChild(img);
+    divImg.appendChild(img);
+    
     // nom 
-    // let nameSearch = name["name"];
     let h2 = document.createElement("h2");
     h2.innerText = name["name"];
     h2.setAttribute("class","name-pokemon");
     card.appendChild(h2);
+
     // id
-    // let id = name["id"];
     let paraId = document.createElement("p");
     paraId.innerText = name["id"];
     paraId.setAttribute("class","id-pokemon")
@@ -38,7 +42,6 @@ function affichage(name) {
 
     // boucle pour récupérer le type 
     for (let i = 0; i< name["types"].length;i++){
-        // let type = name["types"][i].type.name;
         let paraType = document.createElement("p");
         paraType.innerHTML= ` Type : ${name["types"][i].type.name}`
         paraType.setAttribute("class","type-pokemon")
@@ -64,12 +67,14 @@ function affichage(name) {
 // fonction de recherche 
 form.addEventListener("submit",(e) =>{
     e.preventDefault();
-
+    
     if(search.value.length > 0){
+        //on enleve le contenu qui existe 
         card.textContent="";
-        // search.innerHTML=""
+        //appelle de API avec la valeur saisie par l'utilisateur 
         dataPokemon(search.value);
-        // console.log(dataPokemon);
+        //on enleve le texte saisie dans le champs on le renitialisant la variable 
+        search.value=""
         
       
     }
