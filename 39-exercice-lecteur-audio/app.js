049165ddf3d6e7708f238916e2cb8bf87b82d7e8
Tatiana-Juin <tatiana.juinp15@gmail.com>
2023-11-25 17:18:11 +0100

    J'ai créer les fonction funcPrecedent, funcSuivant, funcPlay . Les fonctions funcPrecedent et funcSuivant fonctionne. Je vais creer la fonction funcPlay

--- 39-exercice-lecteur-audio/app.js
+++ 39-exercice-lecteur-audio/app.js
@@ -5,30 +5,30 @@ let precedent = document.querySelector(".precedent");
 let play = document.querySelector(".play");
 let suivant = document.querySelector(".suivant");
 
-const listeAudio = [
-    {
-        nom :'Instrumentale 1',
-        src : 'src/audio-1.mp3'
-    },
-    {
-        nom :'Instrumentale 2',
-        src : 'src/audio-2.mp3'
-    },
-    {
-        nom :'Instrumentale 3',
-        src : 'src/audio-3.mp3'
-    },
-    {
-        nom :'Instrumentale 4',
-        src : 'src/audio-4.mp3'
-    }
-]
+// const listeAudio = [
+//     {
+//         nom :'Instrumentale 1',
+//         src : 'src/audio-1.mp3'
+//     },
+//     {
+//         nom :'Instrumentale 2',
+//         src : 'src/audio-2.mp3'
+//     },
+//     {
+//         nom :'Instrumentale 3',
+//         src : 'src/audio-3.mp3'
+//     },
+//     {
+//         nom :'Instrumentale 4',
+//         src : 'src/audio-4.mp3'
+//     }
+// ]
 
 // POUR LE BOUTON PRECEDENT 
-precedent.addEventListener("click",() =>{
+// precedent.addEventListener("click",() =>{
     
     // console.log(audio.src);
-    let audioSrc = audio.src;
+    // let audioSrc = audio.src;
     // if(audioSrc==="http://127.0.0.1:5500/39-exercice-lecteur-audio/src/audio-2.mp3") {
 
     //     audioSrc="";
@@ -48,5 +48,54 @@ precedent.addEventListener("click",() =>{
     
 
   
-})
+// })
 
+// initialisation de comptPred pour afficher les audio de manière décroissant
+let comptePred = 5;
+
+// fonction pour mettre la chanson précédente
+function funcPrecedent(){
+    // console.log("précédent"); 
+    comptePred --;
+    audio.setAttribute("src","src/audio-"+comptePred+".mp3");
+    titre.innerText="";
+    titre.innerText=`Instrumental ${comptePred}`;
+    if(comptePred === 1){
+        comptePred = 5;
+    }
+    console.log(audio);
+    // console.log(titre);
+}
+// evenement au click sur le bouton précédent 
+precedent.addEventListener("click",funcPrecedent);
+
+
+
+// initialisation de compteSuivant pour afficher les audio de manière incrementer
+let compteSuivant = 1;
+// fonction pour mettre la chanson suivante
+function funcSuivant(){
+    // console.log("suivant");
+    compteSuivant ++;
+    audio.setAttribute("src","src/audio-"+compteSuivant+".mp3");
+    titre.innerText="";
+    titre.innerText=`Instrumental ${compteSuivant}`;
+
+    if(compteSuivant===4){
+        compteSuivant=0;
+    }
+    console.log(audio);
+    // console.log(titre);
+    
+}
+// evenement au click sur le bouton suivant 
+suivant.addEventListener("click",funcSuivant);
+
+// fonction permettant de lancer la chanson et de la mettre en pause
+function funcPlay(){
+
+    console.log("en marche ");
+
+}
+// evenement au click qui appelle la fonction funcPlay
+play.addEventListener("click",funcPlay);
