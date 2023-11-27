681439ac517ad1353c67c47f3ccd9b08548fa024
Tatiana-Juin <tatiana.juinp15@gmail.com>
2023-11-27 14:34:18 +0100

    debut du code js

---
+++ 41-exercice-chrono/app.js
@@ -0,0 +1,6 @@
+// RECUPERER LES ELEMENT DU DOM
+let temps = document.querySelector(".temps");
+let play = document.querySelector(".play");
+let pause = document.querySelector(".pause");
+let reset = document.querySelector(".reset");
+
