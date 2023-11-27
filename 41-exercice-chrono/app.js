c199a0a266aaf9b54aae6c800c6c98e78c47b241
Tatiana-Juin <tatiana.juinp15@gmail.com>
2023-11-27 16:43:34 +0100

    j'ai fini l'exercice

--- 41-exercice-chrono/app.js
+++ 41-exercice-chrono/app.js
@@ -6,6 +6,8 @@ let lesSeconde = document.querySelector(".secondes");
 let play = document.querySelector(".play");
 let pause = document.querySelector(".pause");
 let reset = document.querySelector(".reset");
+let btns = document.querySelector(".btns");
+// console.log(btns);
 // on déclare la variable car on on va clear interval dans chaque fonction
 let interval;
 // VARIABLE INITIALISER
@@ -16,16 +18,33 @@ let seconde = 00;
 function funcPlay() {
   clearInterval(interval);
   interval = setInterval(funcInterval, 10);
+  // si le bouton pause contient la class actibve on l'enleve
+  if(pause.getAttribute("class") == "pause active"){
+    pause.classList.remove("active");
+  }
+  // ajout de la class active au bouton
+  play.classList.add("active");
+
+
+  
 };
 // EVENEMENT QUAND ON APPUYE SUR LE BOUTON  play
 play.addEventListener("click",funcPlay);
 
+
 // FONCTION POUR METTRE EN PAUSE LE CHRONO
 function funcPause() {
   clearInterval(interval);
+  // supprime la class active du bouton play
+  if(play.getAttribute("class") == "play active"){
+    play.classList.remove("active");
+  }
+  // Ajout de la class active
+  pause.classList.add("active");
 };
 // APPELLE LA FONCTION funcPause() QUAND ON CLIQUE SUR LE BOUTON pause
 pause.addEventListener("click",funcPause);
+
 
 // FONCTION POUR RESET LE CHRONO 
 function funcReset() {
@@ -36,15 +55,23 @@ function funcReset() {
   // On affiche le résultat
   lesMiliSeconde.innerHTML = miliSeconde;
   lesSeconde.innerHTML = seconde; 
+
+  // condition pour que le bouton ne soit plus violet
+  if(play.getAttribute("class") == "play active"){
+    play.classList.remove("active");
+  } else{
+    pause.classList.remove("active");
+  }
+  
 };
 // APPELLE DE LA FONCTION funcReset QUAND ON CLIQUE SUR LE BOUTON RESET 
 reset.addEventListener("click",funcReset);
+
 
 // FONCTION POUR LES INTERVAL 
 function funcInterval(){
   // incremente les miliseconde
   miliSeconde++;
-
   //si les miliSeconde sont inferieur a 10
   if (miliSeconde <= 9) {
       lesMiliSeconde.innerHTML = "0" + miliSeconde;
