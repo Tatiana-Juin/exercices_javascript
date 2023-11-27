965e8a97bf670f10174e068dd0e2ca083689a6cc
Tatiana-Juin <tatiana.juinp15@gmail.com>
2023-11-27 14:39:01 +0100

    L'appel des fonctions par rapport au click sur le bouton est fonctionnel

--- 41-exercice-chrono/app.js
+++ 41-exercice-chrono/app.js
@@ -4,3 +4,22 @@ let play = document.querySelector(".play");
 let pause = document.querySelector(".pause");
 let reset = document.querySelector(".reset");
 
+// FONCTION POUR LANCER LE CHRONO
+function funcPlay(){
+
+    console.log("play");
+}
+
+play.addEventListener("click",funcPlay);
+
+// FONCTION POUR METTRE EN PAUSE LE CHRONO
+function funcPause(){
+    console.log("pause");
+}
+pause.addEventListener("click",funcPause);
+
+// FONCTION POUR RESET LE CHRONO 
+function funcReset(){
+    console.log("reset");
+}
+reset.addEventListener("click",funcReset);
