a4c3fe0f9e5841eca50473e82cf34921e9e769ae
Tatiana-Juin <tatiana.juinp15@gmail.com>
2023-11-29 10:57:05 +0100

    le code est fonctionne bien

--- 42-exercice-memory-cards/app.js
+++ 42-exercice-memory-cards/app.js
@@ -9,6 +9,8 @@ let premiereCarte;
 let secondeCarte;
 let compteur = 0;
 
+// let nbFace = 0;
+
 let couleurCarte1, couleurCarte2;
 
 
@@ -26,18 +28,27 @@ for(let i = 0; i< carte.length;i++){
             arriere[i].style.display = "none";
             // on change la valeur de face pour faire apparaitre les images 
             face[i].style.display="block";
+            // ajouter 1 au nb de face
+            // nbFace++;
+            // console.log(`nb face ${nbFace}`);
         }
         else{
             // on fait apparaitre le point d'interrogation 
             arriere[i].style.display = "flex";
             // on fait disparaitre l'image 
             face[i].style.display="none";
+            // enleve un au nbFace
+            // nbFace = nbFace -1;
+            // console.log(`moinsFace ${nbFace}`);
         }
 
 
        /* ********  PARTIE POUR FAIRE LA COMPARAISON DE DEUX CARTE ************** */
         // Si le compteur  est égale a 0
-        if(compteur === 0){
+        // if(nbFace > 0 && nbFace <3){
+
+       
+        if(compteur === 0  ){
             // dans permiereCarte on récupére la valeur du src
             premiereCarte = img[i].getAttribute("src");
             couleurCarte1 = carte[i]
@@ -48,34 +59,56 @@ for(let i = 0; i< carte.length;i++){
             // console.log(premiereCarte);
            
             // return;
+            // console.log(`premiere carte ${compteur}`);
 
         }else{
             // si le compteur est egale a 1
-            if(compteur ===1){
+            if(compteur ===1 ){
 
                 // dans secondeCarte on récupére la valeur du src de l'image 
                 secondeCarte = img[i].getAttribute("src"); 
                 // incremente le compteur
                 compteur++;
                 couleurCarte2 = carte[i];
-                // COMPARAISON DES DEUX CARTES 
-                if(premiereCarte === secondeCarte){
-                    console.log("identique");
-                    // J'ai ajouter la couleur quand c'est bon
-                    couleurCarte1.style.backgroundColor="green";
-                    couleurCarte2.style.backgroundColor="green";
+                // console.log(`seconde carte ${compteur} `);
+
+                // Pour eviter que quand on clique sur la même carte elle ne se compte 
+                if(couleurCarte1.getAttribute("id") === couleurCarte2.getAttribute("id")){
+                    compteur = 0;
+                    // nbFace=0;
+                    console.log(`id ${compteur} `);
+                    
+                }else{
+                    // QUAND LES DEUX SONT IDENTIQUE 
+                    if(premiereCarte === secondeCarte){
+                        console.log("paire");
+                        // J'ai ajouter la couleur quand c'est bon
+                        couleurCarte1.style.backgroundColor="green";
+                        couleurCarte2.style.backgroundColor="green";
+                        // console.log(`couleur ${compteur}`);
+                        // nbFace =0;
+                    }
                 }
-                // on initalise compteur a 0
+
+                
                 compteur = 0;
                 couleurCarte1 = "";
                 couleurCarte2="";
                 // console.log(compteur);
+                // console.log(`fin ${compteur}`);
             }
         }
         // console.log(`dehors de la fonction ${compteur}`);
         
-        // funcCarte()
+        
        
+    //  }
+    //  else{
+    //     nbFace = 0;
+    //     console.log(`initialiser ${nbFace}`);
+    //  }
     });
+
+    
 }
 
--- 42-exercice-memory-cards/idee.md
+++ 42-exercice-memory-cards/idee.md
@@ -10,3 +10,31 @@ face.style.display = "block";
 si 2 image sont identique alors on supprimer le addEventListener dessus 
 résultat la face avant reste visible 
 
+nbFace represente un compteur du nb de carte retourner 
+le but est qu'il en ai pas plus de deux qui est comptabiliser afin d'éviter les ereur 
+
+DANS compteur===1;
+
+
+nbFace = 0; 
+
+Si (compteur ===0) ALORS 
+    premiereCarte = recupere src 
+    couleur = recupérer carte 
+     compteur = compteur +1;
+SINON
+    IF (compteur ===1) ALORS 
+        secondeCarte = recupere url de l'image 
+        couleurCarte2 = recupere carte 
+        compteur = compteur +1
+
+        IF(idCouleurCarte1 === id de couleurCarte2) ALORS 
+            compteur = 0;
+        SINON
+            IF(premiereCarte === secondeCarte) ALORS 
+                couleurCarte1 est vert
+                couleurCarte2 est vert 
+            FSI
+        FSI
+    FSI
+FSI
