bd53c2da000c2f487fc1cca9c258f7a19f39b2e6
Tatiana-Juin <tatiana.juinp15@gmail.com>
2023-11-30 12:38:31 +0100

    J'ai fini de faire le convertisseur

--- 44-exercice-converter-distance/app.js
+++ 44-exercice-converter-distance/app.js
@@ -1,43 +1,62 @@
 // RECUPERE LES ELEMENTS DU DOM 
-let inpConvertir = document.querySelector("#convertir");
-let resultat = document.querySelector("#resultat");
+let input = document.querySelector("#convertir");
 
+let resultat = document.querySelector("#resultat");
 let uniteConvertir = document.querySelector("#unite");
 let uniteResultat = document.querySelector("#resultatUnite");
-
-/* 
-    1 metre = 0,001 kilometre 
-    1000 metre = 1 kilometre
-
-    1metre = 100 centimetre
-    0.01 metre = 1 centimetre 
-
-    100 centimetre = 1kilometre 
-
-    LA CONVERSION
-    0,001 kilometre = 1 m 
-    100 centimetre = 1metre 
-    
-    
-
-*/
 
 
 let valeurUniteConv = uniteConvertir.value;
 let valeurUniteResult = uniteResultat.value;
 
-function resultat(){
+function funcResultat(){
     
     // RECUPERE LES UNITES SELECTIONNERS
     let valeurUniteConv = uniteConvertir.value;
     let valeurUniteResult = uniteResultat.value;
-    // console.log(`unite de depart ${valeurUniteConv} et unite de resultat ${valeurUniteResult}`);
+    
+    // CONVERSION POUR LES METTRE 
+    if (valeurUniteConv==="Metre" && valeurUniteResult==="Kilometre"){
+        resultat.value = Number(input.value) * 0.01;
+        
+    }else if(valeurUniteConv==="Metre" && valeurUniteResult==="Centimetre"){
+        resultat.value = Number(input.value) * 100;
+       
+    } else if(valeurUniteConv==="Metre" && valeurUniteConv==="Metre"){
+        resultat.value = input.value;
+         
+    }
+
+    // CONVERSION POUR LES KILOMETRE 
+    if (valeurUniteConv==="Kilometre" && valeurUniteResult==="Metre"){
+        resultat.value = Number(input.value) * 1000;
+        
+    }else if(valeurUniteConv==="Kilometre" && valeurUniteResult==="Centimetre"){
+        resultat.value = Number(input.value) * 100000;
+       
+    } else if(valeurUniteConv==="Kilometre" && valeurUniteConv==="Kilometre"){
+        resultat.value = input.value;
+         
+    }
+
+    // CONVERSION POUR LES KILOMETRE 
+    if (valeurUniteConv==="Centimetre" && valeurUniteResult==="Metre"){
+        resultat.value = Number(input.value) * 0,00001;
+        
+    }else if(valeurUniteConv==="Centimetre" && valeurUniteResult==="Kilometre"){
+        resultat.value = Number(input.value) * 0.01;
+       
+    } else if(valeurUniteConv==="Centimetre" && valeurUniteConv==="Centimetre"){
+        resultat.value = input.value;
+         
+    }
 
 
 }
+
 // APPELLE DE LA FONCTION QUAND ON SAISI UN NB ET QU'ON APPUYE SUR ENTRE
-inpConvertir.addEventListener("keyup", resultat);
+input.addEventListener("keyup", funcResultat);
 // APPELLE DE LA FONCTION QUAND ON CHANGE D'UNITE POUR LA CONVERSION
-uniteConvertir.addEventListener("change",resultat);
+uniteConvertir.addEventListener("change",funcResultat);
 // APPELLE DE LA FONCTION QUAND ON CHANGE L'UNITE DU RESULTAT
-uniteResultat.addEventListener("change",resultat);
+uniteResultat.addEventListener("change",funcResultat);
