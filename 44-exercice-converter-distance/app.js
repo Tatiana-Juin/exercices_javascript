40f7bbd81e6c6ae6877038ecd412b1c948305f31
Tatiana-Juin <tatiana.juinp15@gmail.com>
2023-11-30 11:32:59 +0100

    debut de la partie JS

---
+++ 44-exercice-converter-distance/app.js
@@ -0,0 +1,20 @@
+// RECUPERE LES ELEMENTS DU DOM 
+let inpConvertir = document.querySelector("#convertir");
+let inpResultat = document.querySelector("#resultat");
+
+let uniteConvertir = document.querySelector("#unite");
+let uniteResultat = document.querySelector("#resultatUnite");
+
+/* 
+    1 metre = 0,001 kilometre 
+    1000 metre = 1 kilometre
+
+    1metre = 100 centimetre
+    0.01 metre = 1 centimetre 
+
+    100 centimetre = 1kilometre 
+
+
+
+*/
+
