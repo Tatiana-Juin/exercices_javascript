e9f5480067916f4012e2196ca44e0e69911b327e
Tatiana-Juin <tatiana.juinp15@gmail.com>
2024-01-15 22:46:54 +0100

    ajout de commentaire

--- exercice-prenom-app/app.js
+++ exercice-prenom-app/app.js
@@ -3,11 +3,11 @@ let h1 =  document.querySelector('h1');
 let nom = document.querySelector("#nom");
 let btn = document.querySelector("#btn");
 
+// EVENEMENT AU CLICK
 btn.addEventListener("click",() =>{
-   
+  //  recupére la valeur
     valeur = nom.value;
-    
-   
+    // Pour l'affichage
    if(valeur.trim() ==""){
      h1.innerText="Bienvenue !";
    }
